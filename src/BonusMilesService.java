8f382c5c509c529f02fbd9f6f82e8ca89e8dfac9
Neto test <d.a.auken-00@mail.ru>
2023-09-11 23:40:39 +0600

    java-task-3-1 cached

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-
     public int calculate(int price) {
-        int result;{
-            result = price/20;
+        int result;
+        {
+            result = price / 20;
         }
         return result;
     }
